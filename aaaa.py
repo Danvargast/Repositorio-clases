c424dfc0396b7ded2f6b6a26e2b61c313e6ddf09
Danvargast <167892315+Danvargast@users.noreply.github.com>
2024-04-23 10:54:08 -0400

    Add files via upload

---
+++ aaaa.py
@@ -0,0 +1,16 @@
+#nombre
+
+print("Bienvenido al mundo de la programación")
+nom = input("Para comenzar, ingresa tu nombre: ")
+print(f"Bienvenido {nom}")
+#ecuación
+
+ent = int(input("Ingrese el número para resolver la ecuación: "))
+print(f"El resultado es: {((ent**2)+(3*ent)+1)/4}")
+
+#nombre rut correo telefono
+name = input("Ingresa tu nombre: ")
+rut = input("Ingresa tu rut: ")
+correo = input("Ingresa tu correo: ")
+telefono = input("Ingresa tu télefono: ")
+print(f"NOMBRE:  \t{name}\nRUT:  \t\t{rut}\nCORREO:  \t{correo}\nTELEFONO: \t{telefono}")
